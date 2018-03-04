4aff3d66db8e16b040e1941d334ee1b2378067cd
Steven Blowers <shdblowers@gmail.com>
2018-03-04 18:16:58 +0000

    extract room row to own component

---
+++ src/RoomsRow.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { get } from 'lodash';
+import { getRoomAvailability } from './accommodation-data';
+import { renderHTML } from './common';
+
+class RoomsTable extends Component {
+  render() {
+    const { room } = this.props;
+
+    const facilities = room.facilities
+      ? room.facilities.map(facility => facility.label).join(', ')
+      : '';
+
+    const price = get(room, 'supplement_price.price.price', '£?');
+
+    const available = getRoomAvailability(room['@id']);
+
+    return (
+      <tr>
+        <td>{room.name}</td>
+        <td>{room.room_type_description}</td>
+        <td>
+          {room['@min_occupancy']} / {room['@occupancy']}
+        </td>
+        <td>{renderHTML(room.description)}</td>
+        <td>{facilities}</td>
+        <td>{price}</td>
+        <td>{available}</td>
+      </tr>
+    );
+  }
+}
+
+export default RoomsTable;
--- src/RoomsTable.js
+++ src/RoomsTable.js
@@ -1,36 +1,10 @@
 import React, { Component } from 'react';
 import { Table } from 'react-bootstrap';
-import { get } from 'lodash';
-import { getRoomAvailability } from './accommodation-data';
-import { renderHTML } from './common';
+import RoomsRow from './RoomsRow';
 
 class RoomsTable extends Component {
   render() {
     const { rooms } = this.props;
-
-    const roomRows = rooms.map((room, index) => {
-      const facilities = room.facilities
-        ? room.facilities.map(facility => facility.label).join(', ')
-        : '';
-
-      const price = get(room, 'supplement_price.price.price', '£?');
-
-      const available = getRoomAvailability(room['@id']);
-
-      return (
-        <tr key={index}>
-          <td>{room.name}</td>
-          <td>{room.room_type_description}</td>
-          <td>
-            {room['@min_occupancy']} / {room['@occupancy']}
-          </td>
-          <td>{renderHTML(room.description)}</td>
-          <td>{facilities}</td>
-          <td>{price}</td>
-          <td>{available}</td>
-        </tr>
-      );
-    });
 
     return (
       <Table bordered hover>
@@ -45,7 +19,9 @@ class RoomsTable extends Component {
             <th className="text-center">Number Available</th>
           </tr>
         </thead>
-        <tbody>{roomRows}</tbody>
+        <tbody>
+          {rooms.map(room => <RoomsRow key={room['@id']} room={room} />)}
+        </tbody>
       </Table>
     );
   }
