7189c55dcae9a129123dfae9d806c3ce76345bb8
Steven Blowers <shdblowers@gmail.com>
2018-03-04 18:19:04 +0000

    fix typo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import AccommodationList from './AccommodationList';
-import { get } from './accommodation-data';
+import { getAccommodation } from './accommodation-data';
 
 class App extends Component {
   render() {
